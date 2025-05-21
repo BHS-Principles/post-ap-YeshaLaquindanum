8ea4c00cfb728e29291e067e3175d2c1645367be
YeshaLaquindanum <2025389@apps.nsd.org>
2025-05-21 16:37:03 +0000

    hi

--- js.js
+++ js.js
@@ -1,13 +1,22 @@
+alert("please work");
+
+
 var list = [
     "frozen burritos",
     "frozen pizza",
     "frozen hotdog",
 ]
 
+var btn = document.getElementById("mybutton")
+var action = function(event){
+    alert("yay");
+};
 
-for(var i = 0;
-    i < 3;
-    i++)
-    {
-        alert("frozenburritos");
+for(var i = 0; i < 3; i++){
+    var newbtn = btn.cloneNode(true);
+    newbtn.innerHTML = list[1];
+    document.body.append(newbtn);
+
+    newbtn.addEventListener("click", action);  
+
     }
